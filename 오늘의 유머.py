f17fadd9584da6122d9c4f33abdd1cc4f3f6d566
youngsoonjo <nolza23@gmail.com>
2023-04-27 11:19:56 +0900

    오늘의 유머 크롤링2

--- 오늘의 유머.py
+++ 오늘의 유머.py
@@ -6,6 +6,8 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+# mode: append+read+write
+f=open("c:\\work\\humor.txt","a+",encoding="utf-8")
 for n in range(1,11):#1~10
         #클리앙의 중고장터 주소 
         data ='https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
@@ -29,9 +31,11 @@ for n in range(1,11):#1~10
  # </span>
 
         for item in list:
-                title=item.find("a").text.strip()
-                #print(title)       
-                if (re.search('한국', title)):
-                        print(title)
+            title=item.find("a").text.strip()
+            #print(title)       
+            if (re.search('한국', title)):
+                print(title)
+                f.write(title+"\n")
+f.close()
 
         
