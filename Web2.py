e25b6696521c4484c182fe5ecb21156d74b54272
youngsoonjo <nolza23@gmail.com>
2023-04-27 10:45:01 +0900

    클리앙 중고장터

--- Web2.py
+++ Web2.py
@@ -4,15 +4,23 @@ import requests
 #크롤링
 from bs4 import BeautifulSoup
 
-url="https://www.daangn.com"
-response=requests.get(url)
-soup=BeautifulSoup(response.text,"html.parser")
 
-posts = soup.find_all("div",attrs={"class":"card-title"})
+url = "https://www.daangn.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+posts = soup.find_all("div", attrs={"class":"card-desc"})
+# 파일에 저장
+f=open("c:\\work\\daangn.txt","wt",encoding="utf-8")
+
 for post in posts:
     title = post.find("h2", attrs={"class":"card-title"})
-    price=post.find("div",attrs={"class":"card-price"})
-    print("{0},{1}".format(title,price))
+    price = post.find("div", attrs={"class":"card-price"})
+    title=title.text.replace("\n","")
+    price=price.text.replace("\n","")
+    print("{0} , {1}".format(title, price))    
+    result =f"매물:{title} 가격: {price} \n"
+    f.write(result)
 
 # <div class="card-desc">
 #       <h2 class="card-title">나이키 신발 팝니다!</h2>
---
+++ 클리앙중고장터검색.py
@@ -0,0 +1,36 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+for n in range(0,10):
+        #클리앙의 중고장터 주소 
+        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
+        #웹브라우져 헤더 추가 
+        req = urllib.request.Request(data, \
+                                    headers = hdr)
+        data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우 다시 해석
+        page = data.decode('utf-8', 'ignore')
+        #2300줄 문자열 넘기는 코드
+        soup = BeautifulSoup(page, 'html.parser')
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+
+ # <span class="subject_fixed" data-role="list-title-text" title="아이패드 9세대 64GB 셀룰러 LTE 단순개봉 새제품급 판매합니다.">
+ # 아이패드 9세대 64GB 셀룰러 LTE 단순개봉 새제품급 판매합니다.
+ # </span>
+
+        for item in list:
+                try:
+                        title=item.text.strip() 
+                        print(title)       
+                        # if (re.search('아이폰', title)):
+                        #         print(title.strip())
+                               
+                except:
+                        pass
+        
